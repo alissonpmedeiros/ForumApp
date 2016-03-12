0059a6c65b7d133189c369b23e516ecef06478c2
alissonpmedeiros <alissonp.medeiros@gmail.com>
2016-03-12 11:55:39 -0300

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.create(params[:comment].permit(:comment))
+  	@comment.user_id = current_user.id
+  	if @comment.save 
+  	  redirect_to post_path(@post)
+  	else
+  	  render :new
+  	end
+  end
+end
